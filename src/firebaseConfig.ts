c1d3f6524800254d3f5e36f0df1804880c1398d6
Aliya <aliya.shaikh@antstack.io>
2024-07-10 17:58:24 +0530

    chore(env): update environment configuration

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,14 +2,14 @@
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
-    apiKey: "AIzaSyCgU4EX5wBrd9Vy7l6zGXb2LGC_9uKNDBA",
-    authDomain: "task-management-system-10ec8.firebaseapp.com",
-    databaseURL: "https://task-management-system-10ec8-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "task-management-system-10ec8",
-    storageBucket: "task-management-system-10ec8.appspot.com",
-    messagingSenderId: "456912992733",
-    appId: "1:456912992733:web:70e3de6442449fd2bede34",
-    measurementId: "G-WFFZNYTQ20"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
