9926f325179eb85ef4ef7c321e6b30c5657ece24
Aliya <aliya.shaikh@antstack.io>
2024-07-11 15:04:42 +0530

    feat: Add NewTask modal with task creation form

--- src/components/NewTask/NewTask.tsx
+++ src/components/NewTask/NewTask.tsx
@@ -1,19 +1,86 @@
 import { useDisclosure } from "@mantine/hooks";
-import { Modal, Button, Textarea } from "@mantine/core";
+import {
+  Modal,
+  Button,
+  Textarea,
+  Flex,
+  Text,
+  TagsInput,
+  OptionsFilter,
+  ComboboxItem,
+  Input,
+} from "@mantine/core";
 import { DateInput, TimeInput } from "@mantine/dates";
 import { useState } from "react";
-import { IconPlus } from "@tabler/icons-react";
+import {
+  IconCalendar,
+  IconCheckbox,
+  IconClock,
+  IconPlus,
+  IconTag,
+  IconUser,
+} from "@tabler/icons-react";
 
 const NewTask = () => {
   const [opened, { open, close }] = useDisclosure(false);
   const [date, setDate] = useState<Date | null>(null);
+  const [tags, setTags] = useState<string[]>([]);
+  const user = ["as", "asd", "asdf"];
+
+  const assigneeFilter: OptionsFilter = ({ options, search }) => {
+    const filtered = (options as ComboboxItem[]).filter((option) =>
+      option.label.toLowerCase().trim().includes(search.toLowerCase().trim())
+    );
+
+    filtered.sort((a, b) => a.label.localeCompare(b.label));
+    return filtered;
+  };
 
   return (
     <>
       <Modal opened={opened} onClose={close} title="Add New Task">
-        <DateInput value={date} onChange={setDate} label="Date" />
-        <TimeInput label="Time" />
-        <Textarea label="Description" />
+        <Flex gap="1rem" direction="column">
+          <Flex align="center" gap="md">
+            <IconCheckbox stroke={1.5} />
+            <Input placeholder="Name of Task" flex={1} />
+          </Flex>
+
+          <Flex align="center" gap="md">
+            <IconCalendar stroke={1.5} />
+            <Text style={{ width: "3rem" }}>Date</Text>
+            <DateInput value={date} onChange={setDate} flex={1} />
+          </Flex>
+          <Flex align="center" gap="md">
+            <IconClock stroke={1.5} />
+            <Text style={{ width: "3rem" }}>Time</Text>
+            <TimeInput flex={1} />
+          </Flex>
+          <Flex align="center" gap="md">
+            <IconTag stroke={1.5} />
+            <Text style={{ width: "3rem" }}>Tags</Text>
+            <TagsInput
+              placeholder="Enter tag"
+              data={[]}
+              value={tags}
+              onChange={setTags}
+              flex={1}
+            />
+          </Flex>
+          <Flex align="center" gap="md">
+            <IconUser stroke={1.5} />
+            <Text style={{ width: "3rem" }}>Assign</Text>
+            <TagsInput
+              placeholder="Enter assignee"
+              data={user}
+              filter={assigneeFilter}
+              flex={1}
+            />
+          </Flex>
+          <Textarea label="Description" />
+          <Button style={{ marginLeft: "auto" }} radius="xl" color="#1D2F6F">
+            Create task
+          </Button>
+        </Flex>
       </Modal>
       <Button
         onClick={open}
