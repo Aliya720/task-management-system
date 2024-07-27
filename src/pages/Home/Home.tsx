314e4d38291737fbf4aa90fc596d8463a7081152
Aliya <aliya.shaikh@antstack.io>
2024-07-27 18:59:37 +0530

    Syle: Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,41 @@
-import { Button, Center } from "@mantine/core";
+import {
+  BackgroundImage,
+  Button,
+  Center,
+  Flex,
+  Group,
+  Text,
+  Title,
+} from "@mantine/core";
 import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
-      <Center>
-        <NavLink to="/sign-in">
-          <Button>Sign In </Button>
-        </NavLink>
-      </Center>
+      <BackgroundImage
+        src="https://pmtips.net/Portals/0/EasyDNNNews/2137/700600p546EDNmainimg-3-types-of-tools-for-project-task-management1.jpg"
+        radius="sm"
+        style={{ height: "100vh" }}
+      >
+        <Flex direction="column" gap="5rem" justify="center" align="center">
+          <Center>
+            <Title order={1} style={{ textAlign: "center" }}>
+              TASK MANAGEMENT SYSTEM{" "}
+              <p> Simplify task management and prioritize work</p>
+            </Title>
+          </Center>
+          <Text>Start Your Task With Us!!!</Text>
+          <Group>
+            <NavLink to="/sign-in">
+              <Button>Sign In </Button>
+            </NavLink>
+
+            <NavLink to="/sign-up">
+              <Button>Get Started </Button>
+            </NavLink>
+          </Group>
+        </Flex>
+      </BackgroundImage>
     </>
   );
 };
